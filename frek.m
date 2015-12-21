9b32629934057cfcc387f4d42b98238e993ecc73
fatihtturan <turan830@gmail.com>
2015-12-22 00:32:33 +0200

    dosyalar guncellendi

--- frek.m
+++ frek.m
@@ -1,4 +1,5 @@
-function(A)=frek(n,o)
-nota=['do','do#','re','mib','mi','fa','fa#','sol','sol#','la','sib','si']
-i=findstr(nota,n);
-A=16,34*2^o*2^(i/12);
+function [C]=frek(nota,oktav)                                               %fonksiyon olusturduk argümanlarý nota ile oktav olan
+notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %notalarý dizilerde belirtiyoruz
+a=strcmp(notalar,nota);                                                     %üstte girilen notayý dizide buluyor
+b=find(a,1);                                                                %girilen notanýn hangi indexte oldugunu buluyor
+C=round(16.35*(2^oktav)*(2^((b-1)/12)));                                    %sayýyý en yakýn deðere kaydýrma
--- note.m
+++ note.m
@@ -1,4 +1,5 @@
 
-function [x,z]=note (A,dur)
-z=0:1/1000000; dur;
-x=sin(2*pi*f*z);
+function [aa,bb]=note (f,dur)   %fonksiyon oluþturduk 
+bb=0:1/1000000:dur;             %frekans aralýgýný belirttik 
+aa=sin(2*pi*f*zz);              %sinüsün hesaplanma fonksiyonu  
+plot(bb,aa)                     %burdan grafiði yazdýrýyoruz 
